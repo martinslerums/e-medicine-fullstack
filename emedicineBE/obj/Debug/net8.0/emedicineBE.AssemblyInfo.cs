74a6eb2d414d7566431353cad491fbb8c482a46e
martleru <68188509+martleru@users.noreply.github.com>
2024-07-12 22:58:25 +0300

    Register

--- emedicineBE/obj/Debug/net8.0/emedicineBE.AssemblyInfo.cs
+++ emedicineBE/obj/Debug/net8.0/emedicineBE.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("emedicineBE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b4e8ee49d5753ebbdf98dacd79f0c1564487fef")]
 [assembly: System.Reflection.AssemblyProductAttribute("emedicineBE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("emedicineBE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
